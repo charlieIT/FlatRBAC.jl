d47d9af314eb65623f75f22823330e3eaf7da651
Luis Oliveira <cljljlj@gmail.com>
2023-01-17 00:58:26 +0000

    match dev updates

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,16 +1,18 @@
 using FlatRBAC
 
 third_party   = Subject(id="3rdPartyApi")
+
 read_database = Permission(name="read_db", resources=["database"], actions=["read", "list"])
-create_key    = Permission("create-key:api,keys:create")
+create_key    = Permission("create-key:api-key:create")
 
 third_party_role = Role(name="3rdPartyApi")
 grant!(third_party_role, read_database, create_key)
 
-# Alternatively, third_party_role = Role(name="3rdPartyApi", permissions=[read_database, create_key])
+# Alternatively, 
+# third_party_role = Role(name="3rdPartyApi", permissions=[read_database, create_key])
 
 grant!(third_party, third_party_role)
 
-@assert isauthorised(third_party, read_database)  "Failed $read_database" # true
-@assert isauthorised(third_party, ":keys:create") "Failed :keys:create" # true
-@assert !isauthorised(third_party, "admin:database:delete") "Failed admin:database:delete" # false
+@assert isauthorised(third_party,  ":database:read")   "Failed :database:read" # true
+@assert isauthorised(third_party,  ":api-key:create")  "Failed :keys:create" # true
+@assert !isauthorised(third_party, ":database:delete") "Failed admin:database:delete" # false
